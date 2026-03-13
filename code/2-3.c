99a3f827bce6145d9b27d6806775945cbd99e60b
eomao <156084917+eomao@users.noreply.github.com>
2026-03-13 14:50:06 +0900

    Update 2-3.c

--- code/2-3.c
+++ code/2-3.c
@@ -4,10 +4,10 @@ int main(){
     unsigned int x = 0x12345678;
     unsigned char* addr = (unsigned char*)&x;
 
-    printf("%c\n", *addr);
-    printf("%c\n", *(addr + 1));
-    printf("%c\n", *(addr + 2));
-    printf("%c\n", *(addr + 3));
+    printf("%02x\n", *addr);
+    printf("%02x\n", *(addr + 1));
+    printf("%02x\n", *(addr + 2));
+    printf("%02x\n", *(addr + 3));
 
     return 0;
 }
