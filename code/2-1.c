047ec97608e2088802d75f7a2d76078df7c6308b
eomao <156084917+eomao@users.noreply.github.com>
2026-03-13 12:15:26 +0900

    2-1
    Added size checks for various data types.

--- code/2-1.c
+++ code/2-1.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
-{
+int main(){
+    printf("char: %zu\n",sizeof(char));
+    printf("short: %zu\n", sizeof(short));
+    printf("int: %zu\n",sizeof(int));
+    printf("unsigned int: %zu\n", sizeof(unsigned int));
+    printf("long: %zu\n",sizeof(long));
+    printf("long long: %zu\n", sizeof(long long));
+    printf("float: %zu\n", sizeof(float)); 
 
     return 0;
 }
