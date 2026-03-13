776790f0bcf9f1038f1144b499770642234391a6
eomao <156084917+eomao@users.noreply.github.com>
2026-03-13 14:27:44 +0900

    Update 4-1.c

--- code/4-1.c
+++ code/4-1.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main()
-{
-
+int main(){
+    int num1;
+    int num2;
+    char ch;
+    scanf("%d %d %c", &num1, &num2, &ch);
+    if(ch == '+'){
+        printf(num1 + num2);
+    }else if(ch == '-'){
+        printf(num1 - num2);
+    }else if(ch == '*'){
+        printf(num1 * num2);
+    }else{
+        printf(num1/num2);
+    }
     return 0;
 }
-
