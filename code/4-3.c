b2d575634c877e92788052dd08394ebc233c265f
eomao <156084917+eomao@users.noreply.github.com>
2026-03-13 14:39:27 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
-int main()
-{
-
+int main(){
+    int num;
+    scanf("%d", &num);
+    int i;
+    for(i = 2; i <= sqrt(num); i++){
+        if(num/i == 0){
+            printf("false");
+            return 0;
+        }
+    }
+    printf("true");
     return 0;
 }
-
